d96161df7622a7a099a39f6431aa297a1b66e8b4
duanhaodong <duanhaodong@sensetime.com>
2020-01-05 21:24:51 +0800

    add flow estimation demo data

--- tools/dist_test.sh
+++ tools/dist_test.sh
@@ -4,4 +4,4 @@ PYTHON=${PYTHON:-"python"}
 
 GPUS=$1
 
-CLUSTER=OFF $PYTHON -m torch.distributed.launch --nproc_per_node=$1 $(dirname "$0")/test.py --launcher pytorch ${@:2}
+CLUSTER=OFF $PYTHON -m torch.distributed.launch --nproc_per_node=$1 $(dirname "$0")/flow_test.py --launcher pytorch ${@:2}
